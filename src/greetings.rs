de6d01e4a8a267efa4d0927ca67876a1b7427f0d
Oluwanifemiiii <93293777+Oluwanifemiiii@users.noreply.github.com>
2023-11-13 20:36:40 +0100

    1st commit

---
+++ src/greetings.rs
@@ -0,0 +1,5 @@
+pub mod spanish;
+pub mod french;
+pub mod english;
+
+
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+///associate greetings module with this crate
+mod greetings;
+mod how_you_hold_data_for_operations;
+///Optionally load each member of greetings
+/*use greetings::default_greeting;
+use greetings::spanish;
+use greetings::french;*/
+///Alternatively, use * to load them all
+//use greetings::*;
+///Alternatively, load them in one line
+
+extern crate hello_world2_lib;
+
+use greetings::{english, spanish, french};
+fn main() {
+let mut greeting: &str = "Hello there";
+greeting = "Hello there again";
+println!("Hello, world!");
+println!("{}", english::default_greeting());
+println!("{}", spanish::default_greeting());
+println!("{}", french::default_greeting());
+}
